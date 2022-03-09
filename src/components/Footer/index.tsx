4bdcca7fa9610a7109520c91c67cc0765716d0fb
ivehement <jvoltci@gmail.com>
2022-03-09 12:24:15 +0530

    Update

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,110 @@
+import { Row, Col } from "antd";
+import { withTranslation } from "react-i18next";
+import { SvgIcon } from "../../common/SvgIcon";
+import Container from "../../common/Container";
+
+import i18n from "i18next";
+import {
+  FooterSection,
+  Title,
+  NavLink,
+  Extra,
+  LogoContainer,
+  Para,
+  Large,
+  Chat,
+  Empty,
+  FooterContainer,
+  Language,
+  Label,
+  LanguageSwitch,
+  LanguageSwitchContainer,
+} from "./styles";
+
+interface SocialLinkProps {
+  href: string;
+  src: string;
+}
+
+const Footer = ({ t }: any) => {
+  const handleChange = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
+  const SocialLink = ({ href, src }: SocialLinkProps) => {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        key={src}
+        aria-label={src}
+      >
+        <SvgIcon src={src} width="25px" height="25px" />
+      </a>
+    );
+  };
+
+  return (
+    <>
+      <FooterSection>
+        <Container>
+
+          <Row justify="space-between">
+            <Col lg={6} md={10} sm={12} xs={12}>
+              <Language>{t("Contact")}</Language>
+              <Large to="/">{t("Tell us everything")}</Large>
+              <Para>
+                {t(`Do you have any question? Feel free to reach out.`)}
+              </Para>
+              <a href="support@altrusian.com">
+                <Chat>{t(`Let's Chat`)}</Chat>
+              </a>
+            </Col>
+            <Col lg={6} md={10} sm={12} xs={12}>
+              <Language>{t("Address")}</Language>
+              <Para>Altrusian LLP, A/2</Para>
+              <Para>Solani Puram, Roorkee</Para>
+              <Para>Uttarakhand - 247667</Para>
+            </Col>
+            <Col lg={6} md={6} sm={12} xs={12}>
+              <Label htmlFor="select-lang">{t("Language")}</Label>
+              <LanguageSwitchContainer>
+                <LanguageSwitch onClick={() => handleChange("en")}>
+                  <SvgIcon
+                    src="united-states.svg"
+                    aria-label="homepage"
+                    width="30px"
+                    height="30px"
+                  />
+                </LanguageSwitch>
+              </LanguageSwitchContainer>
+            </Col>
+          </Row>
+        </Container>
+      </FooterSection>
+      <Extra>
+        <Container border={true}>
+          <Row
+            justify="space-between"
+            align="middle"
+            style={{ paddingTop: "3rem" }}
+          >
+            <NavLink to="/">
+              <LogoContainer>
+                <SvgIcon
+                  src="logo.svg"
+                  aria-label="homepage"
+                  width="101px"
+                  height="64px"
+                />
+              </LogoContainer>
+            </NavLink>
+          </Row>
+        </Container>
+      </Extra>
+    </>
+  );
+};
+
+export default withTranslation()(Footer);
