576edc78e3f8436c662713bdca62f76c8c048851
jvoltci <jvoltci@gmail.com>
2025-07-24 20:05:55 +0530

    fix: minor

--- src/components/BlackHoleButton.tsx
+++ src/components/BlackHoleButton.tsx
@@ -63,6 +63,7 @@ export default function BlackHoleButton() {
       >
         <BlackHoleScene active={active} />
       </div>
+      
 
       <a
         href="https://jvoltci.github.io/ivehement/"
