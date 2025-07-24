8d1fb71ca99bc36d1e19ecea201c511a46802266
jvoltci <jvoltci@gmail.com>
2025-07-24 15:50:35 +0530

    fix: minor

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const isGH = process.env.GITHUB_PAGES === 'true';
 
 const nextConfig = {
-  output: 'export',
-  basePath: isGH ? '/altrusian' : '',
-  assetPrefix: isGH ? '/altrusian/' : '',
+     trailingSlash: true,
+     basePath: isGH ? '/altrusian' : '',
+     output: 'export',
+     images: { unoptimized: true }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
